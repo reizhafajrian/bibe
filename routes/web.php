23a47f290e731eb21213a517ac7547df24e6afba
reizhafajrian <reizha77@gmail.com>
2021-01-06 10:45:59 +0700

    point of sale

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Facade\FlareClient\View;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('view-data', 'AuthorizationController@viewData');
+Route::get('create-data', 'AuthorizationController@createData');
+Route::get('edit-data', 'AuthorizationController@editData');
+Route::get('update-data', 'AuthorizationController@updateData');
+Route::get('delete-data', 'AuthorizationController@deleteData');
+Route::get('/color', function () {
+    return view('colors');
+});
+Route::get('/chart', function () {
+    return view('charts');
+});
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('index');
